8e7ff5490fabddd5002fa4455168d61f724f4388
Iuri Dantas <uri.dantas@hotmail.com>
2023-05-12 18:40:02 -0300

    featcompleted mobile css

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,66 +1,137 @@
 import { useNavigate } from 'react-router-dom';
 import { Container, ContainerContact } from './style';
+import { useEffect } from 'react';
+import { AiOutlineMenu } from 'react-icons/ai';
 import { BsWhatsapp } from 'react-icons/bs';
 
 export function Header() {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    const btnMobile = document.getElementById('btn-mobile');
+    const nav = document.getElementById('nav');
+
+    function toggleMenu(event: MouseEvent) {
+      nav?.classList.toggle('active');
+      const active = nav?.classList.contains('active') ?? false;
+      (event.currentTarget as HTMLElement).setAttribute(
+        'aria-expanded',
+        active.toString(),
+      );
+      if (active) {
+        (event.currentTarget as HTMLElement).setAttribute(
+          'aria-label',
+          'Fechar Menu',
+        );
+      } else {
+        (event.currentTarget as HTMLElement).setAttribute(
+          'aria-label',
+          'Abrir Menu',
+        );
+      }
+    }
+
+    btnMobile?.addEventListener('click', toggleMenu);
+
+    return () => {
+      btnMobile?.removeEventListener('click', toggleMenu);
+    };
+  }, []);
+
+  const navLinks = document.querySelectorAll('#menu a');
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      const nav = document.getElementById('nav');
+      nav?.classList.remove('active');
+    });
+  });
+
   return (
     <Container>
       <img src="/img/logo.png" alt="logo" />
-      <div>
+      <nav id="nav">
         <button
-          onClick={() => {
-            navigate('/');
-          }}
+          id="btn-mobile"
+          aria-label="Abrir Menu"
+          aria-haspopup="true"
+          aria-controls="menu"
+          aria-expanded="false"
         >
-          Home
+          <AiOutlineMenu />
         </button>
-        <button
-          onClick={() => {
-            navigate('/services');
-          }}
-        >
-          Serviços
-        </button>
-        <button
-          onClick={() => {
-            navigate('/onlinConsulting');
-          }}
-        >
-          Consultoria Online
-        </button>
-        <button
-          onClick={() => {
-            navigate('/plans');
-          }}
-        >
-          Planos
-        </button>
-        <button
-          onClick={() => {
-            navigate('/testimonials');
-          }}
-        >
-          Depoimentos
-        </button>
-        <button
-          onClick={() => {
-            navigate('/contact');
-          }}
-        >
-          Contato
-        </button>
-      </div>
+        <ul id="menu" role="menu">
+          <li>
+            <a
+              onClick={() => {
+                navigate('/');
+              }}
+              className="container-link"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                navigate('/serviços');
+              }}
+              className="container-link"
+            >
+              Serviços
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                navigate('/consultoria-online');
+              }}
+              className="container-link"
+            >
+              Consultoria Online
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                navigate('/planos');
+              }}
+              className="container-link"
+            >
+              Planos
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                navigate('/depoimentos');
+              }}
+              className="container-link"
+            >
+              Depoimentos
+            </a>
+          </li>
+          <li>
+            <a
+              onClick={() => {
+                navigate('/contato');
+              }}
+              className="container-link"
+            >
+              Contato
+            </a>
+          </li>
+        </ul>
+      </nav>
       <ContainerContact>
         <a
           href="https://api.whatsapp.com/send?phone=5579998014413"
           target="_blank"
-          rel="noopener noreferrer"
+          className="contact-link"
         >
           <BsWhatsapp size={24} />
+          <h2>79 9 9801-4413</h2>
         </a>
-        <h2>79 9 9801-4413</h2>
       </ContainerContact>
     </Container>
   );
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -2,41 +2,96 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
+  padding: 2rem 5rem;
   width: 100%;
-  padding: 20px 0 20px 0;
-  display: flex;
   background-color: rgb(48 48 48);
 
   img {
-    width: 150px;
-    height: 150px;
+    width: 152px;
+    height: 152px;
   }
 
-  button {
+  ul {
+    padding: 0px;
+    display: flex;
+    list-style: none;
+    gap: 1rem;
+  }
+
+  .container-link {
+    display: block;
     background: none;
+    cursor: pointer;
     color: #ffff97;
-    font-size: 1rem;
+    font-size: 1.5rem;
     border-radius: 8px;
     width: auto;
-    padding: 5px;
-    margin-right: 20px;
+    padding: 0.5rem;
     border: none;
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
     transform: translate(0px, 0px);
     transition: all 0.3s ease-in-out;
   }
 
-  button:last-of-type {
+  .container-link:last-of-type {
     margin-right: 0;
   }
 
-  button:hover {
+  .container-link:hover {
     background-color: #000000;
     color: #ffff97;
     box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.35);
     transform: translate(0px, -2px);
+  }
+
+  button {
+    display: none;
+  }
+
+  @media (max-width: 600px) {
+    padding: 2rem 1rem;
+
+    img {
+      width: 152px;
+      height: 152px;
+    }
+
+    ul {
+      display: block;
+      position: absolute;
+      background-color: rgb(48 48 48);
+      width: 100%;
+      top: 216px;
+      right: 0;
+      height: 0;
+      transition: 0.6s;
+      z-index: 1000;
+      visibility: hidden;
+      overflow-y: hidden;
+    }
+
+    nav.active ul {
+      height: calc(100vh - 183px);
+      visibility: visible;
+      overflow-y: auto;
+    }
+
+    a {
+      font-size: 1.4rem;
+      padding: 1rem 0;
+      margin: 0 1rem;
+    }
+
+    button {
+      display: flex;
+      font-size: 2rem;
+      background: none;
+      color: #ffff97;
+      border: none;
+      cursor: pointer;
+    }
   }
 `;
 
@@ -52,10 +107,14 @@ export const ContainerContact = styled.div`
     margin-left: 10px;
   }
 
-  a {
+  a.contact-link {
     display: flex;
     align-items: center;
     text-decoration: none;
     color: #ffff97;
+  }
+
+  @media (max-width: 600px) {
+    display: none;
   }
 `;
